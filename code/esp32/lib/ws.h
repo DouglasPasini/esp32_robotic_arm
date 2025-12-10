56b16a6569422d6cba84e94944c6b861c47f485c
Douglas Pasini <dpasini40023001@gmail.com>
2025-12-10 09:48:48 -0300

    Update ws.h

--- code/esp32/lib/ws.h
+++ code/esp32/lib/ws.h
@@ -10,10 +10,22 @@
 using namespace websockets;
 extern WebsocketsClient webSocket;
 
+// --- Inicialização e conexão do WebSocket ---
+
 void wsInit();
 void wsConnect();
+
+// --- Tratamento de mensagens ---
+
 void onEventsCallback(WebsocketsEvent event, String data);
 void onMessageCallback(WebsocketsMessage message);
+
+// --- Envio de mensagens ---
+
+void sendSensor(const char *id, int state); // função que envia estado do sensor
+
+// --- Loop ---
+
 void webSocketRun();
 
 #endif
